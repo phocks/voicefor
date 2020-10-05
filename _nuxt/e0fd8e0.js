09a1a3038290e666c05725bbb0bc2f445c3d3e4e
phocks <phocks@users.noreply.github.com>
2020-10-05 20:47:34 +0000

    deploy: c80463c62fc2886495abbbded52eb76d8e935b52

--- _nuxt/e0fd8e0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{196:function(e,n,t){"use strict";t.r(n);var l=t(31),component=Object(l.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("Oh yeah!!!")])}),[],!1,null,null,null);n.default=component.exports}}]);
