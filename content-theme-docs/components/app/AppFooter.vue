f1e8b1b70a85abac6a0d31c5ff6d5e17c1e2b8e1
Joshua Byrd <phocks@gmail.com>
2020-10-11 08:30:41 +1000

    Built on

--- content-theme-docs/components/app/AppFooter.vue
+++ content-theme-docs/components/app/AppFooter.vue
@@ -1,10 +1,15 @@
 <template>
-  <footer class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-20">
-    <div class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between">
+  <footer
+    class="h-16 relative w-full border-t dark:border-gray-800 bg-white dark:bg-gray-900 z-20"
+  >
+    <div
+      class="h-full w-full container mx-auto px-4 lg:px-8 flex items-center justify-between"
+    >
       <div class="flex items-end">
         <a href="https://nuxtjs.org" target="_blank" rel="noopener">
-          <BuiltWithNuxtLight class="h-4 light-img" />
-          <BuiltWithNuxtDark class="h-4 dark-img" />
+          built with nuxt.org
+          <!-- <BuiltWithNuxtLight class="h-4 light-img" />
+          <BuiltWithNuxtDark class="h-4 dark-img" /> -->
         </a>
       </div>
       <div class="flex">
@@ -18,9 +23,9 @@
 <script>
 export default {
   computed: {
-    availableLocales () {
-      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
-    }
-  }
-}
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale);
+    },
+  },
+};
 </script>
