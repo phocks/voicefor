1bb7df63d275f9b659debe707d3cc6ae2a7dd68b
Joshua Byrd <phocks@gmail.com>
2020-10-11 13:30:15 +1000

    iCon?

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 import theme from "./content-theme-docs";
 
 export default theme({
+  pwa: {
+    icon: {
+      /* icon options */
+      source: "[staticDir]/icon.png"
+    }
+  }
   // head: {
   //   link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
   // },
